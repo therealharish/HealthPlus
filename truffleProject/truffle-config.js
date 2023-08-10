40892e3b09a02f9dd142fd5af738583f29dfce51
Harish <48938639+therealharish@users.noreply.github.com>
2023-08-10 11:06:06 +0530

    first

--- truffleProject/truffle-config.js
+++ truffleProject/truffle-config.js
@@ -4,10 +4,15 @@ const { INFURA_API_KEY, MNEMONIC } = process.env;
 
 module.exports = {
   networks: {
-    sepolia: {
-      provider: () => new HDWalletProvider(MNEMONIC, INFURA_API_KEY),
-      network_id: "11155111",
-      gas: 4465030,
+    // sepolia: {
+    //   provider: () => new HDWalletProvider(MNEMONIC, INFURA_API_KEY),
+    //   network_id: "11155111",
+    //   gas: 4465030,
+    // },
+    ganache: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "5777"
     },
   },
   compilers: {
